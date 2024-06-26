204990ac759100e4160c71c86f14b83e1c7066d3
Himanshu Mittal <33717149+iamhkm@users.noreply.github.com>
2024-06-27 00:19:02 +0530

    Export urls feature (#8)
    * adding support for exporting urls
    
    * adding download url button on dashboard

--- backend/src/util/constants.js
+++ backend/src/util/constants.js
@@ -3,6 +3,7 @@ export const USER_ROLE_BASIC = "BASIC";
 export const USER_ROLE_PRIME = "PRIME";
 export const BASIC_USER_PROJECTION = "unique_id,email,added_date,total_url,total_active";
 export const BASIC_URL_PROJECTION = "unique_id,user_id,original_url,added_date,last_modified,url_status,total_hit,short_url,identification_name,description,tag";
+export const EXPORT_URL_PROJECTION = "unique_id,identification_name,original_url,short_url,url_status,total_hit";
 export const MATRIX_TYPE_USER = "USER"
 export const SEND_STATS_VIA_EMAIL = (process.env.SEND_STATS_VIA_EMAIL) ? true : false;
 export const ADD_URL_STATS_KEY = "add_stats";
--- backend/src/util/s3Util.js
+++ backend/src/util/s3Util.js
@@ -1,9 +1,8 @@
-import fs from 'fs';
-import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+import { S3Client, PutObjectCommand, GetObjectCommand } from '@aws-sdk/client-s3';
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 export async function uploadBufferToS3 (buffer, s3Key, contentType){
     const s3Client = new S3Client();
-    // const fileContent = buffer.toString('base64');
     const params = {
         Bucket: process.env.STATS_BUCKET,
         Key: s3Key,
@@ -16,5 +15,20 @@ export async function uploadBufferToS3 (buffer, s3Key, contentType){
         console.log("File uploaded successfully");
     } catch (err) {
         console.error(`Error uploading file: ${err.message}`);
+    }
+}
+
+export async function getObjectUrl (s3Key){
+    const s3Client = new S3Client();
+    const params = {
+        Bucket: process.env.STATS_BUCKET,
+        Key: s3Key
+    };
+    try {
+        const command = new GetObjectCommand(params);
+        const signedUrl = await getSignedUrl(s3Client, command, { expiresIn: 3600 });
+        return signedUrl;
+    } catch (err) {
+        throw new Error(err.message);
     }
 }
